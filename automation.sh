c728d8defd4b588411ad6ab5b9f7c5ea766f5596
v33skey <vvoskresensky62@gmail.com>
2026-04-29 16:16:29 +0700

    Implement SSH key generation prompt and option validation

--- automation.sh
+++ automation.sh
@@ -18,24 +18,50 @@ is_valid_ip() {
     return 0
 }
 
-while true; do
-    read -rp "Enter server IP: " SERVER_IP
+#UNCOMMENT THIS
+# while true; do
+#     read -rp "Enter server IP: " SERVER_IP
 
-    if is_valid_ip "$SERVER_IP"; then
-        break
-    fi
+#     if is_valid_ip "$SERVER_IP"; then
+#         break
+#     fi
 
-    printf "Invalid IP, try again. \n"
+#     printf "Invalid IP, try again. \n"
 
-done
+# done
 
-read -rp "Enter username: " USERNAME
+# read -rp "Enter username: " USERNAME
 
-read -rsp "Enter user password: " PASSWD
+# read -rsp "Enter user password: " PASSWD
 
-printf "\n "
+#DELETE THIS
+SERVER_IP="192.168.0.1"
+USERNAME="USER"
+PASSWD="123123"
+
+printf "\n"
 printf "Connection parameters:\n"
 printf "Server IP: %s\n" "$SERVER_IP"
 printf "Username: %s\n" "$USERNAME"
 
+#Asks user need SSH-key
+printf "Do you need to create SSH-key? (Print 1 or 2)\n"
+printf "1. Yes\n"
+printf "2. No\n"
+
+
+while true; do
+    read -rp "Choose an option (Default Yes): " SSH_OPTION
+
+    if [ "$SSH_OPTION" = "1" ] || [ -z "$SSH_OPTION" ]; then
+        ssh-keygen -t ed25519 -f ~/.ssh/keykey
+        break
+    elif [ "$SSH_OPTION" = "2" ]; then
+        printf "Now you need to login by your password every time\n"
+        break
+    else
+        printf "Invalid option. Try again.\n"s
+    fi
+done
+#script exiting
 read -rsp "Press any key to close window..."
